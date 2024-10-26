ad9bcca46d0ca09fe6e7f211f34d4eb385414a7a
Gustavo Marques <guto.gutomarques@outlook.com>
2024-10-26 00:26:22 -0300

    launch: Component CaixaEletronico

---
+++ src/app/components/CaixaEletronico.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+// import Teclado from './Teclado';
+// import TelaOperacao from './TelaOperacao';
+
+export default function CaixaEletronico(){
+    const [tela, setTela] = useState('inicio');
+
+    const handleOperacao = (operacao) => {
+        switch (operacao) {
+            case 'pix';
+                setTela('pix');
+                break;
+            case 'saque';
+                setTela('saque');
+                break;
+            case 'extrato';
+                setTela('extrato');
+                break;
+            case 'deposito';
+                setTela('deposito');
+              break;
+            case 'cancelar';
+              setTela('inicio');
+              break;
+            default:
+                setTela('inicio');
+        }
+    };
+
+    return (
+        <div className="flex flex-col items-center p-6 bg-gray-500 h-screen text-white">
+            <TelaOperacao tela={tela} />
+            <Teclado onOperacao={handleOperacao} />
+        </div>
+    );
+}
